8e3d3c25bbc3cde172843c1fbd3db6848308f11f
jaquiflan <124392609+jaquiflan@users.noreply.github.com>
2023-02-03 12:42:22 -0600

    Add files via upload

---
+++ EVA_1_HOLAMUNDO/src/eva_1_holamundo/EVA_1_HOLAMUNDO.java
@@ -0,0 +1,12 @@
+package eva_1_holamundo;
+public class EVA_1_HOLAMUNDO {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println("hola mundo!!");
+    }
+    
+    public static void algo(){
+        System.out.println();
+}
+    
+}
