6def1caadb7492f2b472edba79823c21be5d52d5
jaquiflan <124392609+jaquiflan@users.noreply.github.com>
2023-03-06 00:54:29 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/main/java/com/mycompany/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_12_alcance;
+
+/**
+ *
+ * @author campa
+ */
+public class EVA1_12_ALCANCE {
+
+    public static void main(String[] args) {
+        //System.out.println(x);
+        //la x no existe si se declara despues de la instruccion
+        int x = 100;
+        System.out.println(x);
+    }
+}
