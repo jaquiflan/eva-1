8df135596242d34b8383c2f2c64711196cb86183
jaquiflan <124392609+jaquiflan@users.noreply.github.com>
2023-03-06 00:25:33 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/main/java/com/mycompany/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_7_metodos_1;
+
+/**
+ *
+ * @author campa
+ */
+public class EVA1_7_METODOS_1 {
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+    public static void myMethod(){
+        System.out.println("You have called me! My name is myMethod");
+    }
+    //cuando se llama al metodo, el programa brinca al metodo
+}
