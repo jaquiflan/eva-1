66c13c49f18bf61fc3665ed4c4ee23c88f92d2c0
jaquiflan <124392609+jaquiflan@users.noreply.github.com>
2023-03-06 00:58:52 -0600

    Add files via upload

---
+++ EVA1_13_ALCANCE2/src/main/java/com/mycompany/eva1_13_alcance2/EVA1_13_ALCANCE2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_13_alcance2;
+
+/**
+ *
+ * @author campa
+ */
+public class EVA1_13_ALCANCE2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+            int x = 100;
+            System.out.println(x);
+            {
+                int y = 100;
+                System.out.println(y);
+            }
+        }
+        System.out.println(i);
+        //para que esto funcione se debe declarar la variable dentro del for
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
