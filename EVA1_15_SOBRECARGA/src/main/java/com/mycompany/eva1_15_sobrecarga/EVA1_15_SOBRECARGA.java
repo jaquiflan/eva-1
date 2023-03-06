3917cc9955d55c02e33b4740f8d4e91e9a051151
jaquiflan <124392609+jaquiflan@users.noreply.github.com>
2023-03-06 01:07:39 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/main/java/com/mycompany/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_15_sobrecarga;
+
+/**
+ *
+ * @author campa
+ */
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        //metodos con el mismo nombre, diferente firma(atributos)
+        System.out.println("HELLO WORLD!!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    public static void algo(){
+        
+    }
+    public static void algo(int x){
+        
+    }
+}
